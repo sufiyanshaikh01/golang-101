cf69252f88c194b2b5fd377429f68b2e314b2d01
SUFIYAN SHAIKH <softwaremastar0010@gmail.com>
2025-01-25 21:26:10 +0530

    basic.go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Lear Go Language by Hello World")
+
+	//aaj veriable bhi start ho gaya aur sumjh bhi aa gaya
+	// variable esay hai
+
+	var person = "Sufiyan"
+	fmt.Println(person)
+	var version = 89
+	fmt.Println(version)
+
+	var age = 19
+	fmt.Println(age)
+
+	const name = "SUFIYAN SHAIKH"
+	fmt.Println(name)
+
+	//myutil.PrintMessage("hiii My name is Sufiyan")
+
+}
